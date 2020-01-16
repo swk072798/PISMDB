89c1c8ceb1a6acae8bc554042ad2724aca352622
swk072798 <474052231@qq.com>
2020-01-16 17:49:48 +0800

    修改

--- src/main/java/com/nwafu/PISMDB/controller/PathwaysController.java
+++ src/main/java/com/nwafu/PISMDB/controller/PathwaysController.java
@@ -52,22 +52,10 @@ public class PathwaysController {
     @RequestMapping(value = "/showAllPathways",method = RequestMethod.GET)
     @ResponseBody
     public String showPicsAll(@RequestParam String callback) {
-        long startTime = System.currentTimeMillis();
         List<Pic> list1 = pathwaysService.showPictures();
         List<BrowsePathways> pathways = new ArrayList<>();
         for(int i=0;i<pathwaysService.getPicturesCount();i++){
-            List<Pictures> listPre = pathwaysService.showPictureInformation(list1.get(i).getId());
-//            log.info("listPre:{}",listPre);
-            List<Pictures> list = new ArrayList<>();
-            for(Pictures p : listPre){
-                if(p != null){
-                    if(p.getGroupName() != null){
-                        p.setGroupName(p.getGroupName().trim());
-                    }
-                    list.add(p);
-                }
-            }
-//            log.info("pictires:{}",list);
+            List<Pictures> list = pathwaysService.showPictureInformation(list1.get(i).getId());
             BrowsePathways p1 = new BrowsePathways();
             List<GroupFormat> groupFormats=new ArrayList<>();
             for(int j=0;j<list.size();j++){
@@ -76,11 +64,8 @@ public class PathwaysController {
                 if(list.get(j).getMolecularPISMID()!= null) {
                     groupFormat.setId(Arrays.asList(list.get(j).getMolecularPISMID().split("%%")));
                 }
-                if(groupFormat.getGroupName() != null || groupFormat.getId() != null){
-                    groupFormats.add(groupFormat);
-                }
+                groupFormats.add(groupFormat);
             }
-            log.info("compoundsGroup:{}",groupFormats);
             List<GroupFormat> groupFormat1 = new ArrayList<>();
             for (int k = 0; k < list.size(); k++) {
                 GroupFormat groupFormat = new GroupFormat();
@@ -88,10 +73,7 @@ public class PathwaysController {
                 if (list.get(k).getProteinTargetID() != null) {
                     groupFormat.setId(Arrays.asList(list.get(k).getProteinTargetID().split("%%")));
                 }
-                if(groupFormat.getId() != null || groupFormat.getGroupName() != null){
-                    groupFormat1.add(groupFormat);
-                }
-                log.info("proteinInfo:{}",groupFormat1);
+                groupFormat1.add(groupFormat);
             }
             p1.setCompoundGroup(groupFormats);
             p1.setPic(list1.get(i));
@@ -99,8 +81,6 @@ public class PathwaysController {
             p1.setProteinGroup(groupFormat1);
             pathways.add(p1);
         }
-        log.info("调用showPicsAll，用时 {} ms",System.currentTimeMillis() - startTime);
-        log.info("返回值为：{}",pathways);
         CallbackResult<List<BrowsePathways>> result = new CallbackResult();
         result.setCallback(callback);
         result.setData(pathways);
@@ -117,6 +97,7 @@ public class PathwaysController {
     @RequestMapping(value = "/show",method = RequestMethod.GET)
     @ResponseBody
     public String showPics(@RequestParam String callback) {
+
         List<Pic> list1 = pathwaysService.showPictures();
         List<Pictures> list = pathwaysService.showPictureInformation(list1.get(2).getId());
         List<BrowsePathways> pathways = new ArrayList<>();
